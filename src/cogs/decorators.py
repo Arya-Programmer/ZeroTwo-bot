dcd9097792bfddc13c0b9ebe006b7ca9e4442b91
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-05-09 01:54:27 -0800

    decotrators file bug fix

--- src/cogs/decorators.py
+++ src/cogs/decorators.py
@@ -5,12 +5,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_DIR = os.path.join(BASE_DIR, "../info.db")
 
 
-def connectToDB(func):
+async def connectToDB(func):
     db = sqlite3.connect(DB_DIR)
     cursor = db.cursor()
 
-    def wrapper(*args, **kwargs):
-        return func(*args, cursor, **kwargs)
+    async def wrapper(*args, **kwargs):
+        return await func(*args, cursor, **kwargs)
 
     cursor.close()
     db.close()
